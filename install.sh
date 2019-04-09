abb5760e980781447ab7f2e9338ed8dcf63da77d
=xianyunyehe <tianlei542475736@gmail.com>
2019-04-10 00:30:02 +0800

    add ubuntu support

--- install.sh
+++ install.sh
@@ -4,16 +4,70 @@ set -e
 os_name=`uname -s`
 machine=`uname -m`
 PWD_PATH=`pwd`
+if grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    DISTRO='RHEL'
+elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+    DISTRO='RHEL'
+elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+    DISTRO='Debian'
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    DISTRO='Debian'
+else
+    DISTRO='unknow'
+fi
 #chromium_verison 版本号
 chromium_verison='599821'
 local_chromium_dir=$PWD_PATH"/node_modules/puppeteer/.local-chromium"
-if [[ "$os_name" == "Linux" ]]; then
+if [[ "$DISTRO" == "RHEL" ]]; then
     download_url="https://npm.taobao.org/mirrors/chromium-browser-snapshots/Linux_x64/${chromium_verison}/chrome-linux.zip"
     #依赖库
 	yum install pango.x86_64 libXcomposite.x86_64 libXcursor.x86_64 libXdamage.x86_64 libXext.x86_64 libXi.x86_64 libXtst.x86_64 cups-libs.x86_64 libXScrnSaver.x86_64 libXrandr.x86_64 GConf2.x86_64 alsa-lib.x86_64 atk.x86_64 gtk3.x86_64 wget -y
 	#字体
 	yum install ipa-gothic-fonts xorg-x11-fonts-100dpi xorg-x11-fonts-75dpi xorg-x11-utils xorg-x11-fonts-cyrillic xorg-x11-fonts-Type1 xorg-x11-fonts-misc -y
 	local_chromium_dir=${local_chromium_dir}"/linux-"${chromium_verison}"/"
+elif [[ "$DISTRO" == "Debian" ]]; then
+    sudo apt install -f \
+            gconf-service \
+            libasound2 \
+            libatk1.0-0 \
+            libatk-bridge2.0-0 \
+            libc6 \
+            libcairo2 \
+            libcups2 \
+            libdbus-1-3 \
+            libexpat1 \
+            libfontconfig1 \
+            libgcc1 \
+            libgconf-2-4 \
+            libgdk-pixbuf2.0-0 \
+            libglib2.0-0 \
+            libgtk-3-0 \
+            libnspr4 \
+            libpango-1.0-0 \
+            libpangocairo-1.0-0 \
+            libstdc++6 \
+            libx11-6 \
+            libx11-xcb1 \
+            libxcb1 \
+            libxcomposite1 \
+            libxcursor1 \
+            libxdamage1 \
+            libxext6 \
+            libxfixes3 \
+            libxi6 \
+            libxrandr2 \
+            libxrender1 \
+            libxss1 \
+            libxtst6 \
+            ca-certificates \
+            fonts-liberation \
+            libappindicator1 \
+            libnss3 \
+            lsb-release \
+            xdg-utils \
+            wget
+    download_url="https://npm.taobao.org/mirrors/chromium-browser-snapshots/Mac/${chromium_verison}/chrome-mac.zip"
+    local_chromium_dir=${local_chromium_dir}"/linux-"${chromium_verison}"/"
 elif [[ "$os_name" == "Darwin" ]]; then
     download_url="https://npm.taobao.org/mirrors/chromium-browser-snapshots/Mac/${chromium_verison}/chrome-mac.zip"
     local_chromium_dir=${local_chromium_dir}"/mac-"${chromium_verison}"/"
